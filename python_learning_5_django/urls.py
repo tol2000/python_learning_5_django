2da2e957ff7975d0d88ee37533a4417fb469d3d8
tol2000 <tolicheg@gmail.com>
2020-11-05 22:51:48 +0200

    homepage downloaded static files such as css, js from bootstrap

--- python_learning_5_django/urls.py
+++ python_learning_5_django/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+
+
+def redirect_to_default_app(request):
+    return redirect('homepage/')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', redirect_to_default_app),
     path('homepage/', include('homepage.urls'))
 ]
