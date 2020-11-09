c65cfd1336b7b9e71ad0e0401d020074d2cd292a
tol2000 <tolicheg@gmail.com>
2020-11-09 22:36:10 +0200

    lesson 6 up to filters in templates

--- homepage/views.py
+++ homepage/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.views.decorators.http import require_GET
-import datetime
+from datetime import datetime, timedelta
 
 
 def index_page(request: HttpRequest):
-    date_now = datetime.datetime.today()
+    date_now = datetime.today()
     cookie_last_visit_name = 'last_visit'
-    last_visit = request.get_signed_cookie(cookie_last_visit_name, None)
-    response = render(request, 'homepage/index.html')
+    # last_visit = request.get_signed_cookie(cookie_last_visit_name, None)
+    days = [
+        date_now - timedelta(days=x) for x in range(35)
+    ]
+    args = {
+        'days': days,
+        'days_str': '\n'.join([str(x) for x in days])
+    }
+    response = render(request, 'homepage/index.html', args)
     response.set_cookie(cookie_last_visit_name, date_now)
     return response
 
@@ -16,7 +23,7 @@ def index_page(request: HttpRequest):
 @require_GET
 def articles(request: HttpRequest):
     articles_list = [
-        f'Article # {x}' for x in range(1, 8)
+        x for x in range(1, 8)
     ]
     args = {
         'articles': articles_list,
