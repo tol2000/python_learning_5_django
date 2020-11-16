0c1f2098ec12681132c72cc034c0702505bf183c
tol2000 <tolicheg@gmail.com>
2020-11-16 22:19:31 +0200

    lesson 6 up to url parameters

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,5 +5,6 @@ app_name = 'homepage'
 
 urlpatterns = [
     path('', views.index_page, name='index'),
-    path('articles/', views.articles, name='articles')
+    path('articles/', views.articles, name='articles'),
+    path('articles/<str:article_id>', views.article, name='articles')
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.views.decorators.http import require_GET
@@ -8,14 +10,7 @@ def index_page(request: HttpRequest):
     date_now = datetime.today()
     cookie_last_visit_name = 'last_visit'
     # last_visit = request.get_signed_cookie(cookie_last_visit_name, None)
-    days = [
-        date_now - timedelta(days=x) for x in range(35)
-    ]
-    args = {
-        'days': days,
-        'days_str': '\n'.join([str(x) for x in days])
-    }
-    response = render(request, 'homepage/index.html', args)
+    response = render(request, 'homepage/index.html')
     response.set_cookie(cookie_last_visit_name, date_now)
     return response
 
@@ -24,7 +19,7 @@ def index_page(request: HttpRequest):
 def articles(request: HttpRequest):
     date_now = datetime.today()
     articles_list = [
-        str(date_now - timedelta(days=x)) for x in range(35)
+        (x.year, x.month, x.day, x.hour, x.minute) for x in [date_now - timedelta(days=x) for x in range(35)]
     ]
     args = {
         'articles': articles_list,
@@ -32,3 +27,12 @@ def articles(request: HttpRequest):
             f'{request.method} to host {request.get_host()}'
     }
     return render(request, 'homepage/articles.html', args)
+
+
+@require_GET
+def article(request: HttpRequest, article_id):
+    args = {
+        'article_id': article_id,
+        'article_text': '\n'.join([f'{x} - article_id' for x in range(1, random.randint(50, 100))])
+    }
+    return render(request, 'homepage/article.html', args)
