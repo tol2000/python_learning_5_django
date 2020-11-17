032f4d68fef8bbee9a0db9ec0ddc27e6019d1ede
tol2000 <tolicheg@gmail.com>
2020-11-17 12:50:18 +0200

    lesson 6 up to class-based views

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'homepage'
 
 urlpatterns = [
-    path('', views.index_page, name='index'),
+    path('', TemplateView.as_view(template_name='homepage/index.html'), name='index'),
     path('articles/', views.articles, name='articles'),
-    path('articles/<str:article_id>', views.article, name='articles')
+    path('articles/<str:article_id>', views.ArticleView.as_view(), name='articles')
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,25 +1,37 @@
 import random
 
-from django.shortcuts import render
 from django.http import HttpRequest
+from django.shortcuts import render
 from django.views.decorators.http import require_GET
 from datetime import datetime, timedelta
 
+from django.views.generic import TemplateView
 
-def index_page(request: HttpRequest):
-    date_now = datetime.today()
-    cookie_last_visit_name = 'last_visit'
-    # last_visit = request.get_signed_cookie(cookie_last_visit_name, None)
-    response = render(request, 'homepage/index.html')
-    response.set_cookie(cookie_last_visit_name, date_now)
-    return response
+
+class ArticleView(TemplateView):
+    template_name = 'homepage/article.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        args = {
+            'article_id': kwargs['article_id'],
+            'article_text':
+                '\n'.join(
+                    [
+                        f'{x} - {", ".join(["Lorem ipsum" for y in range(1, random.randint(8, 10))])}'
+                        for x in range(1, random.randint(50, 100))
+                    ]
+                )
+        }
+        context.update(args)
+        return context
 
 
 @require_GET
 def articles(request: HttpRequest):
     date_now = datetime.today()
     articles_list = [
-        (x.year, x.month, x.day, x.hour, x.minute) for x in [date_now - timedelta(days=x) for x in range(35)]
+        (x.year, x.month, x.day, x.hour, x.minute, x.second) for x in [date_now - timedelta(days=x) for x in range(35)]
     ]
     args = {
         'articles': articles_list,
@@ -27,12 +39,3 @@ def articles(request: HttpRequest):
             f'{request.method} to host {request.get_host()}'
     }
     return render(request, 'homepage/articles.html', args)
-
-
-@require_GET
-def article(request: HttpRequest, article_id):
-    args = {
-        'article_id': article_id,
-        'article_text': '\n'.join([f'{x} - article_id' for x in range(1, random.randint(50, 100))])
-    }
-    return render(request, 'homepage/article.html', args)
