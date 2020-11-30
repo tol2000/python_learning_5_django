ae8177060316f95cb8e52337686e1008e0097065
a.kliusa <a.kliusa@ppl33-35.com>
2020-11-30 15:20:06 +0200

    corrected settings

--- python_learning_5_django/settings/local.py
+++ python_learning_5_django/settings/local.py
@@ -1,3 +1,4 @@
+# noinspection PyUnresolvedReferences
 from python_learning_5_django.settings.base import *
 
 # SECURITY WARNING: Do not set to true on prod!
--- python_learning_5_django/settings/prod.py
+++ python_learning_5_django/settings/prod.py
@@ -1,9 +1,10 @@
-from python_learning_5_django.settings import base
+# noinspection PyUnresolvedReferences
+from python_learning_5_django.settings.base import *
 
 # SECURITY WARNING: Keep the key in secret place!
-base.SECRET_KEY = open('secret')
+SECRET_KEY = open('secret')
 
 # SECURITY WARNING: Do not set to true on prod!
-base.DEBUG = False
+DEBUG = False
 
-base.ALLOWED_HOSTS = ['myhost.com']
+ALLOWED_HOSTS = ['myhost.com']
